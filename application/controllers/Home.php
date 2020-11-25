49be34610a71b220a2a397f95a3f93c380035dec
allii98 <allii98>
2020-11-25 11:30:45 +0700

    penambahan dashboard

---
+++ application/controllers/Home.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+
+    public function index()
+    {
+        $data = [
+            'tittle'          => 'Dashboard',
+        ];
+
+        $this->template->load('template', 'dashboard', $data);
+    }
+}
+
+/* End of file Home.php */

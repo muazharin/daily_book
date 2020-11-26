f54c1a5d9480c00dff7348f6d1d12e25afd00bb3
muazharin <alfanmuazharin@gmail.com>
2020-11-26 15:45:15 +0700

    url untuk submit request

--- daily_book/lib/model/baseurl.dart
+++ daily_book/lib/model/baseurl.dart
@@ -2,4 +2,5 @@ class Baseurl {
   static String ip = "http://172.16.5.92/daily_book/api";
   static String userdaftar = "$ip/user";
   static String login = "$ip/login";
+  static String submitRequest = "$ip/submitRequest";
 }
